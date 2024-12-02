a2f6ce300bddc6a391c7b8f5b075d913e238a509
Andrey Lezhen <andreylezhen@mail.ru>
2024-12-02 14:51:58 +0500

    first commit

---
+++ LezhenAI_6.1/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using LezhenAI_6_1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LezhenAI_6._1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void OnNavigateButtonClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+        }
+    }
+}
